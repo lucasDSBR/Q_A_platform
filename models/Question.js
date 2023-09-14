a124eaf020b0d588d8acdad915c0f07331f02c01
Lucas da Silva Maciel <lucas.silva@olostech.com>
2023-09-14 20:19:00 -0300

    feature: Create views and install sequelize

---
+++ models/Question.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const connection = require('../database/database');
+
+const Question  = connection.define('questions', {
+	title: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	description: {
+		type: Sequelize.TEXT,
+		allowNull: false
+	}
+});
+Question.sync({force: false}).then(() => {})
+
+module.exports = Question;
